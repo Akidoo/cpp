0b12a1b24a71ecc575e13c06e4833b7988e04b95
Akidoo <90885452+Akidoo@users.noreply.github.com>
2023-03-17 15:17:40 +0800

    first commit

---
+++ C++/helloWorld/helloworld.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //imports input/output functionality
+
+int main() { //entry point to the program
+    std::cout << "Hello World"; //std::cout allows us to output 1 or more characters. std = standard library
+    return 0;
+}
